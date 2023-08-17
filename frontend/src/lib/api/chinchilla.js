da561dbfd8964649dfdd09ec0901c61d52a4fae0
ponchoay <poncho1122c@gmail.com>
2023-08-17 18:33:01 +0900

    add:チンチラプロフィールページに画像更新機能を追加

--- frontend/src/lib/api/chinchilla.js
+++ frontend/src/lib/api/chinchilla.js
@@ -47,7 +47,8 @@ export const updateChinchilla = ({ chinchillaId, params }) => {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),
-      uid: Cookies.get('_uid')
+      uid: Cookies.get('_uid'),
+      'content-type': 'multipart/form-data'
     }
   })
 }
