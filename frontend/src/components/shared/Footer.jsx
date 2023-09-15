d8eeb3fdd322628e4c3d96c6b4c09d5bd6f09ef7
ponchoay <poncho1122c@gmail.com>
2023-09-15 23:51:46 +0900

    add:フッターにお世話記録表示ページに遷移するアイコンを追加

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays, faHouse } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import { AuthContext } from 'src/contexts/auth'
 
@@ -11,9 +11,14 @@ export const Footer = () => {
     <footer className="fixed bottom-0 z-10 h-16 w-full bg-dark-blue">
       <div className="mx-auto flex h-full max-w-screen-lg place-content-evenly items-center">
         {isSignedIn && currentUser ? (
-          <Link href="/mychinchilla">
-            <FontAwesomeIcon icon={faHouse} className="text-4xl text-ligth-white" />
-          </Link>
+          <div>
+            <Link href="/mychinchilla">
+              <FontAwesomeIcon icon={faHouse} className="mx-12 text-4xl text-ligth-white" />
+            </Link>
+            <Link href="/care-record-calendar">
+              <FontAwesomeIcon icon={faCalendarDays} className="mx-12 text-4xl text-ligth-white" />
+            </Link>
+          </div>
         ) : (
           <></>
         )}
