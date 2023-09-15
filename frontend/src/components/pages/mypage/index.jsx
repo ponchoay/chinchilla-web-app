a46ef5647d3c59c5e18412496cc228df8ca4b967
ponchoay <poncho1122c@gmail.com>
2023-09-15 22:21:44 +0900

    remove:不要なコードを削除

--- frontend/src/components/pages/mypage/index.jsx
+++ frontend/src/components/pages/mypage/index.jsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Cookies from 'js-cookie'
 import { signOut } from 'src/lib/api/auth'
