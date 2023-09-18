c62ae087f2931c2259488cab7771b956e77c6942
ponchoay <poncho1122c@gmail.com>
2023-09-18 15:36:16 +0900

    add:チンチラ登録ページにバリデーションを追加

--- frontend/src/components/pages/chinchilla-registration/index.jsx
+++ frontend/src/components/pages/chinchilla-registration/index.jsx
@@ -2,6 +2,10 @@ import { useRef, useCallback, useState } from 'react'
 import { useRouter } from 'next/router'
 import { createChinchilla } from 'src/lib/api/chinchilla'
 
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { chinchillaSchema } from 'src/validation/chinchilla'
+
 import { Button } from 'src/components/shared/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAsterisk, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
@@ -9,11 +13,21 @@ import { faAsterisk, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 export const ChinchillaRegistrationPage = () => {
   const router = useRouter()
   const [chinchillaImage, setChinchillaImage] = useState('')
-  const [chinchillaName, setChinchillaName] = useState('')
-  const [chinchillaSex, setChinchillaSex] = useState('')
-  const [chinchillaBirthday, setChinchillaBirthday] = useState('')
-  const [chinchillaMetDay, setChinchillaMetDay] = useState('')
   const chinchillaMemo = ''
+
+  const {
+    register,
+    handleSubmit,
+    formState: { dirtyFields, errors }
+  } = useForm({
+    defaultValues: {
+      chinchillaName: '',
+      chinchillaSex: '',
+      chinchillaBirthday: '',
+      chinchillaMetDay: ''
+    },
+    resolver: zodResolver(chinchillaSchema)
+  })
 
   // プレビュー用
   const [previewImage, setPreviewImage] = useState('')
@@ -44,21 +58,20 @@ export const ChinchillaRegistrationPage = () => {
   }, [])
 
   // FormData形式でデータを作成
-  const createFormData = () => {
+  const createFormData = (data) => {
     const formData = new FormData()
     formData.append('chinchilla[chinchillaImage]', chinchillaImage)
-    formData.append('chinchilla[chinchillaName]', chinchillaName)
-    formData.append('chinchilla[chinchillaSex]', chinchillaSex)
-    formData.append('chinchilla[chinchillaBirthday]', chinchillaBirthday)
-    formData.append('chinchilla[chinchillaMetDay]', chinchillaMetDay)
+    formData.append('chinchilla[chinchillaName]', data.chinchillaName)
+    formData.append('chinchilla[chinchillaSex]', data.chinchillaSex)
+    formData.append('chinchilla[chinchillaBirthday]', data.chinchillaBirthday)
+    formData.append('chinchilla[chinchillaMetDay]', data.chinchillaMetDay)
     formData.append('chinchilla[chinchillaMemo]', chinchillaMemo)
     return formData
   }
 
   // チンチラプロフィール作成機能
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const params = createFormData()
+  const onSubmit = async (data) => {
+    const params = createFormData(data)
     try {
       const res = await createChinchilla(params)
       console.log(res)
@@ -75,102 +88,116 @@ export const ChinchillaRegistrationPage = () => {
       alert('エラーです')
     }
   }
+
   return (
     <div className="my-40 grid place-content-center place-items-center">
       <p className="text-center text-2xl font-bold tracking-widest text-dark-blue">
         チンチラの登録
       </p>
-      <div className="relative">
-        <button type="button" onClick={handleClickChangeImage} className=" mt-6">
-          <img
-            src={previewImage ? previewImage : '/images/default.svg'}
-            alt="プロフィール画像"
-            className="h-[200px] w-[200px] rounded-3xl border border-solid border-dark-blue bg-ligth-white"
-          />
-          <FontAwesomeIcon
-            icon={faCirclePlus}
-            className="absolute bottom-[0px] right-[0px] z-10 rounded-[50%] bg-ligth-white text-4xl text-dark-blue"
-          />
-        </button>
-      </div>
-      <input
-        type="file"
-        accept="image/jpg,image/jpeg,image/gif,image/png"
-        onChange={handleUpload}
-        ref={imageInputRef}
-        className="file-input file-input-bordered file-input-primary hidden w-full max-w-xs"
-      />
-      <div className="form-control mt-6 w-96">
-        <label htmlFor="chinchillaName" className="label">
-          <span className="text-base text-dark-black">名前</span>
-          <div>
-            <FontAwesomeIcon icon={faAsterisk} className="mr-1 text-xs text-dark-pink" />
-            <span className="label-text-alt text-dark-black">必須入力</span>
-          </div>
-        </label>
-        <input
-          id="chinchillaName"
-          type="text"
-          placeholder="チンチラの名前"
-          value={chinchillaName}
-          onChange={(event) => setChinchillaName(event.target.value)}
-          className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
-        />
-      </div>
-      <div className="form-control mt-6 w-96">
-        <label htmlFor="chinchillaSex" className="label">
-          <span className="text-base text-dark-black">性別</span>
-          <div>
-            <FontAwesomeIcon icon={faAsterisk} className="mr-1 text-xs text-dark-pink" />
-            <span className="label-text-alt text-dark-black">必須入力</span>
-          </div>
-        </label>
-        <select
-          id="chinchillaSex"
-          value={chinchillaSex}
-          onChange={(event) => setChinchillaSex(event.target.value)}
-          className="w-ful select select-bordered select-primary border-dark-blue bg-ligth-white text-base font-light text-dark-black"
-        >
-          <option hidden value="">
-            選択してください
-          </option>
-          <option value="オス">オス</option>
-          <option value="メス">メス</option>
-          <option value="不明">不明</option>
-        </select>
-      </div>
-      <div className="form-control mt-6 w-96">
-        <label htmlFor="chinchillaBirthday" className="label">
-          <span className="text-base text-dark-black">誕生日</span>
-        </label>
-        <input
-          id="chinchillaBirthday"
-          type="date"
-          value={chinchillaBirthday}
-          onChange={(event) => setChinchillaBirthday(event.target.value)}
-          className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
-        />
-      </div>
-      <div className="form-control mb-12 mt-6 w-96">
-        <label htmlFor="chinchillaMetDay" className="label">
-          <span className="text-base text-dark-black">お迎え日</span>
-        </label>
-        <input
-          id="chinchillaMetDay"
-          type="date"
-          value={chinchillaMetDay}
-          onChange={(event) => setChinchillaMetDay(event.target.value)}
-          className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
-        />
-      </div>
-      <Button
-        type="submit"
-        click={handleSubmit}
-        disabled={!chinchillaName || !chinchillaSex ? true : false}
-        addStyle="btn-primary h-16 w-40"
+      <form
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid place-content-center place-items-center"
       >
-        登録
-      </Button>
+        <div className="relative">
+          <button type="button" onClick={handleClickChangeImage} className=" mt-6">
+            <img
+              src={previewImage ? previewImage : '/images/default.svg'}
+              alt="プロフィール画像"
+              className="h-[200px] w-[200px] rounded-3xl border border-solid border-dark-blue bg-ligth-white"
+            />
+            <FontAwesomeIcon
+              icon={faCirclePlus}
+              className="absolute bottom-[0px] right-[0px] z-10 rounded-[50%] bg-ligth-white text-4xl text-dark-blue"
+            />
+          </button>
+        </div>
+        <input
+          type="file"
+          accept="image/jpg,image/jpeg,image/gif,image/png"
+          onChange={handleUpload}
+          ref={imageInputRef}
+          className="file-input file-input-bordered file-input-primary hidden w-full max-w-xs"
+        />
+        <div className="form-control mt-6 h-32 w-96">
+          <label htmlFor="chinchillaName" className="label">
+            <span className="text-base text-dark-black">名前</span>
+            <div>
+              <FontAwesomeIcon icon={faAsterisk} className="mr-1 text-xs text-dark-pink" />
+              <span className="label-text-alt text-dark-black">必須入力</span>
+            </div>
+          </label>
+          <input
+            id="chinchillaName"
+            type="text"
+            {...register('chinchillaName')}
+            placeholder="チンチラの名前"
+            className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
+          />
+          {errors.chinchillaName && (
+            <p className="label text-base text-dark-pink">{errors.chinchillaName.message}</p>
+          )}
+        </div>
+        <div className="form-control mt-6 h-32 w-96">
+          <label htmlFor="chinchillaSex" className="label">
+            <span className="text-base text-dark-black">性別</span>
+            <div>
+              <FontAwesomeIcon icon={faAsterisk} className="mr-1 text-xs text-dark-pink" />
+              <span className="label-text-alt text-dark-black">必須入力</span>
+            </div>
+          </label>
+          <select
+            id="chinchillaSex"
+            {...register('chinchillaSex')}
+            className="w-ful select select-bordered select-primary border-dark-blue bg-ligth-white text-base font-light text-dark-black"
+          >
+            <option hidden value="">
+              選択してください
+            </option>
+            <option value="オス">オス</option>
+            <option value="メス">メス</option>
+            <option value="不明">不明</option>
+          </select>
+          {errors.chinchillaSex && (
+            <p className="label text-base text-dark-pink">{errors.chinchillaSex.message}</p>
+          )}
+        </div>
+        <div className="form-control mt-6 h-32 w-96">
+          <label htmlFor="chinchillaBirthday" className="label">
+            <span className="text-base text-dark-black">誕生日</span>
+          </label>
+          <input
+            id="chinchillaBirthday"
+            type="date"
+            {...register('chinchillaBirthday')}
+            className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
+          />
+          {errors.chinchillaBirthday && (
+            <p className="label text-base text-dark-pink">{errors.chinchillaBirthday.message}</p>
+          )}
+        </div>
+        <div className="form-control mb-12 mt-6 h-32 w-96">
+          <label htmlFor="chinchillaMetDay" className="label">
+            <span className="text-base text-dark-black">お迎え日</span>
+          </label>
+          <input
+            id="chinchillaMetDay"
+            type="date"
+            {...register('chinchillaMetDay')}
+            className="w-ful input input-bordered input-primary input-md border-dark-blue bg-ligth-white text-base text-dark-black"
+          />
+          {errors.chinchillaMetDay && (
+            <p className="label text-base text-dark-pink">{errors.chinchillaMetDay.message}</p>
+          )}
+        </div>
+        <Button
+          type="submit"
+          disabled={!dirtyFields.chinchillaName || !dirtyFields.chinchillaSex ? true : false}
+          addStyle="btn-primary h-16 w-40"
+        >
+          登録
+        </Button>
+      </form>
     </div>
   )
 }
---
+++ frontend/src/validation/chinchilla.js
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+
+export const chinchillaSchema = z
+  .object({
+    chinchillaName: z
+      .string()
+      .nonempty('名前は必須です')
+      .max(15, '名前は15文字以下で入力してください'),
+    chinchillaSex: z.string().nonempty('性別は必須です'),
+    chinchillaBirthday: z.string().optional(),
+    chinchillaMetDay: z.string().optional()
+  })
+  // 第一引数の条件がfalseの場合に、第二引数のメッセージを表示
+  .refine((data) => !data.chinchillaBirthday || new Date(data.chinchillaBirthday) <= new Date(), {
+    message: '誕生日は未来の日付に設定できません',
+    path: ['chinchillaBirthday']
+  })
+  .refine((data) => !data.chinchillaMetDay || new Date(data.chinchillaMetDay) <= new Date(), {
+    message: 'お迎え日は未来の日付に設定できません',
+    path: ['chinchillaMetDay']
+  })
+  .refine(
+    (data) =>
+      !data.chinchillaBirthday ||
+      !data.chinchillaMetDay ||
+      new Date(data.chinchillaBirthday) <= new Date(data.chinchillaMetDay),
+    {
+      message: 'お迎え日は誕生日以降の日付で設定してください',
+      path: ['chinchillaMetDay']
+    }
+  )
