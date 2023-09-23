a4ffa64a59960a77a8f287d2d29b8308d20cc869
ponchoay <poncho1122c@gmail.com>
2023-09-24 01:19:11 +0900

    add:マイチンチラページにバリデーションを追加

--- frontend/src/validation/chinchilla.js
+++ frontend/src/validation/chinchilla.js
@@ -29,3 +29,46 @@ export const chinchillaRegistrationSchema = z
       path: ['chinchillaMetDay']
     }
   )
+
+export const chinchillaProfileSchema = z
+  .object({
+    chinchillaName: z
+      .string()
+      .nonempty('名前は必須です')
+      .max(15, '名前は15文字以下で入力してください'),
+    chinchillaSex: z.string().nonempty('性別は必須です'),
+    chinchillaBirthday: z.string().nullable(),
+    chinchillaMetDay: z.string().nullable(),
+    chinchillaMemo: z.string().max(200, '200文字以下で入力してください')
+  })
+  // 第一引数の条件がfalseの場合に、第二引数のメッセージを表示
+  .refine(
+    (data) =>
+      !data.chinchillaBirthday ||
+      new Date(data.chinchillaBirthday) <=
+        new Date(),
+    {
+      message: '誕生日は未来の日付に設定できません',
+      path: ['chinchillaBirthday']
+    }
+  )
+  .refine(
+    (data) =>
+      !data.chinchillaMetDay ||
+      new Date(data.chinchillaMetDay)<=
+        new Date(),
+    {
+      message: 'お迎え日は未来の日付に設定できません',
+      path: ['chinchillaMetDay']
+    }
+  )
+  .refine(
+    (data) =>
+      !data.chinchillaBirthday ||
+      !data.chinchillaMetDay ||
+      new Date(data.chinchillaBirthday) <= new Date(data.chinchillaMetDay),
+    {
+      message: 'お迎え日は誕生日以降の日付で設定してください',
+      path: ['chinchillaMetDay']
+    }
+  )
