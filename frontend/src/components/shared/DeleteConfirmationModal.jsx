3fa06fb4b1632d414a0b78a240a2a9b08b0cd0bf
ponchoay <poncho1122c@gmail.com>
2023-10-10 16:44:31 +0900

    add:削除確認用のモーダルを追加

---
+++ frontend/src/components/shared/DeleteConfirmationModal.jsx
@@ -0,0 +1,26 @@
+import { Button } from 'src/components/shared/Button'
+
+export const DeleteConfirmationModal = ({ setIsModalOpen, handleDelete }) => {
+  return (
+    <>
+      {/* 削除確認モーダル */}
+      <div className="fixed inset-0 flex h-full w-full items-center justify-center  bg-gray-400/50">
+        <div className="z-10 grid h-1/5 w-1/2 place-content-center place-items-center rounded-lg bg-ligth-white p-5">
+          <p className="text-lg text-dark-black">本当に削除しますか？</p>
+          <div className="mt-8 py-3">
+            <Button
+              type="button"
+              click={() => setIsModalOpen(false)}
+              addStyle="btn-primary w-32 h-14 mr-10"
+            >
+              キャンセル
+            </Button>
+            <Button type="submit" click={handleDelete} addStyle="btn-secondary h-14 w-32">
+              削除
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
