72cd2316eb0cefb73238f1151ad44bf64a27e81e
ponchoay <poncho1122c@gmail.com>
2023-10-12 12:35:19 +0900

    add:体重のページを追加

---
+++ frontend/src/components/pages/weight-chart/index.jsx
@@ -0,0 +1,7 @@
+export const WeightChartPage = () => {
+  return (
+    <div className="my-40 grid place-content-center place-items-center">
+      <p className="text-center text-2xl font-bold tracking-widest text-dark-blue">体重</p>
+    </div>
+  )
+}
---
+++ frontend/src/pages/weight-chart.jsx
@@ -0,0 +1,9 @@
+import { WeightChartPage } from 'src/components/pages/weight-chart'
+
+export default function SignUp() {
+  return (
+    <>
+      <WeightChartPage />
+    </>
+  )
+}
