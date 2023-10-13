313ad6db3d335734370e4f428c63afd2f9a7bca4
ponchoay <poncho1122c@gmail.com>
2023-10-14 01:05:15 +0900

    add:体重のグラフを追加

--- frontend/src/components/pages/weight-chart/index.jsx
+++ frontend/src/components/pages/weight-chart/index.jsx
@@ -1,7 +1,16 @@
+import dynamic from 'next/dynamic'
+
+const DynamicWeightChart = dynamic(
+  () =>
+    import('src/components/pages/weight-chart/weightChart').then((module) => module.WeightChart),
+  { ssr: false }
+)
+
 export const WeightChartPage = () => {
   return (
     <div className="my-40 grid place-content-center place-items-center">
       <p className="text-center text-2xl font-bold tracking-widest text-dark-blue">体重</p>
+      <DynamicWeightChart />
     </div>
   )
 }
