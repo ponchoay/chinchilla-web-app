98fb662935ed24ddc1b1651c6e618da0e59b72c0
ponchoay <poncho1122c@gmail.com>
2023-10-15 18:41:55 +0900

    add:DBの体重データをグラフに表示

--- frontend/src/components/pages/weight-chart/index.jsx
+++ frontend/src/components/pages/weight-chart/index.jsx
@@ -1,16 +1,59 @@
 import dynamic from 'next/dynamic'
+import { useState, useEffect, useContext } from 'react'
+import { getAllChinchillas } from 'src/lib/api/chinchilla'
+import { getWeightCares } from 'src/lib/api/care'
+import { SelectedChinchillaIdContext } from 'src/contexts/chinchilla'
 
-const DynamicWeightChart = dynamic(
-  () =>
-    import('src/components/pages/weight-chart/weightChart').then((module) => module.WeightChart),
-  { ssr: false }
-)
+import { ChinchillaSelectFormItem } from 'src/components/pages/weight-chart/chinchillaSelectFormItem'
 
 export const WeightChartPage = () => {
+  const [allChinchillas, setAllChinchillas] = useState([])
+  const [allWeightCares, setAllWeightCares] = useState([])
+
+  const { chinchillaId, setChinchillaId } = useContext(SelectedChinchillaIdContext)
+
+  const DynamicWeightChart = dynamic(
+    () =>
+      import('src/components/pages/weight-chart/weightChart').then((module) => module.WeightChart),
+    { ssr: false }
+  )
+
+  // 全てのチンチラのデータを取得
+  const fetch = async () => {
+    const res = await getAllChinchillas()
+    console.log(res.data)
+    setAllChinchillas(res.data)
+  }
+
+  // // 初回レンダリング時に全てのチンチラのデータを取得
+  useEffect(() => {
+    fetch()
+  }, [])
+
+  // チンチラを選択し、お世話記録の一覧を取得
+  const handleGetChinchilla = async (event) => {
+    // selectedChinchillaIdはこの関数の中だけで使うchinchillaId
+    const selectedChinchillaId = event.target.value
+    setChinchillaId(selectedChinchillaId)
+    try {
+      const res = await getWeightCares(selectedChinchillaId)
+      console.log('体重記録一覧：', res.data)
+      setAllWeightCares(res.data)
+    } catch (err) {
+      console.log(err)
+      alert('エラーです')
+    }
+  }
+
   return (
     <div className="my-40 grid place-content-center place-items-center">
       <p className="text-center text-2xl font-bold tracking-widest text-dark-blue">体重</p>
-      <DynamicWeightChart />
+      <DynamicWeightChart allWeightCares={allWeightCares} />
+      <ChinchillaSelectFormItem
+        chinchillaId={chinchillaId}
+        handleGetChinchilla={handleGetChinchilla}
+        allChinchillas={allChinchillas}
+      />
     </div>
   )
 }
--- frontend/src/components/pages/weight-chart/weightChart.jsx
+++ frontend/src/components/pages/weight-chart/weightChart.jsx
@@ -1,47 +1,26 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
-const rowData = [
-  { id: 1, careDay: '2023-09-01', careWeight: 500 },
-  { id: 2, careDay: '2023-09-02', careWeight: 560 },
-  { id: 3, careDay: '2023-09-03', careWeight: 540 },
-  { id: 4, careDay: '2023-09-04', careWeight: 560 },
-  { id: 5, careDay: '2023-09-05', careWeight: 530 },
-  { id: 6, careDay: '2023-09-06', careWeight: 490 },
-  { id: 7, careDay: '2023-09-07', careWeight: 550 },
-  { id: 8, careDay: '2023-09-08', careWeight: 490 },
-  { id: 9, careDay: '2023-09-09', careWeight: 490 },
-  { id: 10, careDay: '2023-09-10', careWeight: 490 },
-  { id: 11, careDay: '2023-09-11', careWeight: 560 },
-  { id: 12, careDay: '2023-09-12', careWeight: 490 },
-  { id: 13, careDay: '2023-09-13', careWeight: 490 },
-  { id: 14, careDay: '2023-09-14', careWeight: 670 },
-  { id: 15, careDay: '2023-09-15', careWeight: 490 },
-  { id: 16, careDay: '2023-09-16', careWeight: 500 },
-  { id: 17, careDay: '2023-09-18', careWeight: 550 },
-  { id: 18, careDay: '2023-09-20', careWeight: 530 },
-  { id: 19, careDay: '2023-09-22', careWeight: 540 },
-  { id: 20, careDay: '2023-09-29', careWeight: 525 },
-  { id: 21, careDay: '2023-09-30', careWeight: 500 }
-]
 
-const careWeightDataList = rowData.map((item) => ({
-  ...item,
-  careDay: new Date(item.careDay).getTime()
-}))
+export const WeightChart = ({ allWeightCares }) => {
+  // 日付をDate型に変換
+  const careWeightDataList = allWeightCares.map((item) => ({
+    ...item,
+    careDay: new Date(item.careDay).getTime()
+  }))
 
-const CustomTooltip = ({ active, payload, label }) => {
-  if (active && payload && payload.length) {
-    const date = new Date(label)
-    return (
-      <div className="mx-auto h-[70px] w-[70px] items-center rounded border bg-white p-2 text-center shadow">
-        <p className="text-base text-dark-black">{`${date.getMonth() + 1}/${date.getDate()}`}</p>
-        <p className="text-base text-dark-black">{`${payload[0].value}g`}</p>
-      </div>
-    )
+  // ツールチップの表示形式をカスタマイズ
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      const date = new Date(label)
+      return (
+        <div className="mx-auto h-[70px] w-[70px] items-center rounded border bg-white p-2 text-center shadow">
+          <p className="text-base text-dark-black">{`${date.getMonth() + 1}/${date.getDate()}`}</p>
+          <p className="text-base text-dark-black">{`${payload[0].value}g`}</p>
+        </div>
+      )
+    }
+    return null
   }
-  return null
-}
 
-export const WeightChart = () => {
   return (
     <LineChart
       width={700}
