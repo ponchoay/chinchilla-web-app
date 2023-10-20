f624843ed41ceb2c3af85584f7cd0e7ddce02354
ponchoay <poncho1122c@gmail.com>
2023-10-20 15:28:42 +0900

    add:平均体重と記録の数を表示する枠を追加

--- frontend/src/components/pages/weight-chart/index.jsx
+++ frontend/src/components/pages/weight-chart/index.jsx
@@ -41,46 +41,63 @@ export const WeightChartPage = () => {
   return (
     <div className="my-40 grid place-content-center place-items-center">
       <p className="text-center text-2xl font-bold tracking-widest text-dark-blue">体重</p>
+
+      {/* グラフ */}
       <DynamicWeightChart allWeightCares={allWeightCares} timeRange={timeRange} />
 
-      <div className="mt-10">
-        <div className="join">
-          <input
-            id="1month"
-            type="radio"
-            name="options"
-            onChange={() => setTimeRange('1month')}
-            aria-label="過去1か月"
-            checked={chinchillaId && timeRange === '1month'}
-            className="btn join-item"
-          />
-          <input
-            id="6months"
-            type="radio"
-            name="options"
-            onChange={() => setTimeRange('6months')}
-            aria-label="過去6か月"
-            checked={chinchillaId && timeRange === '6months'}
-            className="btn join-item"
-          />
-          <input
-            id="1year"
-            type="radio"
-            name="options"
-            onChange={() => setTimeRange('1year')}
-            aria-label="過去1年間"
-            checked={chinchillaId && timeRange === '1year'}
-            className="btn join-item"
-          />
-          <input
-            id="all"
-            type="radio"
-            name="options"
-            onChange={() => setTimeRange('all')}
-            aria-label="全期間"
-            checked={chinchillaId && timeRange === 'all'}
-            className="btn join-item px-7"
-          />
+      {/* 表示範囲のラジオボタン */}
+      <div className="join mt-10">
+        <input
+          id="1month"
+          type="radio"
+          name="options"
+          onChange={() => setTimeRange('1month')}
+          aria-label="過去1か月"
+          checked={chinchillaId && timeRange === '1month'}
+          className="btn join-item"
+        />
+        <input
+          id="6months"
+          type="radio"
+          name="options"
+          onChange={() => setTimeRange('6months')}
+          aria-label="過去6か月"
+          checked={chinchillaId && timeRange === '6months'}
+          className="btn join-item"
+        />
+        <input
+          id="1year"
+          type="radio"
+          name="options"
+          onChange={() => setTimeRange('1year')}
+          aria-label="過去1年間"
+          checked={chinchillaId && timeRange === '1year'}
+          className="btn join-item"
+        />
+        <input
+          id="all"
+          type="radio"
+          name="options"
+          onChange={() => setTimeRange('all')}
+          aria-label="全期間"
+          checked={chinchillaId && timeRange === 'all'}
+          className="btn join-item px-7"
+        />
+      </div>
+
+      {/* まとめ */}
+      <div className="mt-6 h-[150px] w-[400px] rounded-xl bg-ligth-white p-3">
+        <div className="mx-10 mt-5 flex items-center border-b border-solid border-b-light-black pb-2">
+          <p className="w-28 text-center text-base text-dark-black">平均体重</p>
+          <div className="flex grow justify-evenly text-center">
+            <p className="text-center text-base text-dark-black">500g</p>
+          </div>
+        </div>
+        <div className="mx-10 mt-5 flex items-center border-b border-solid border-b-light-black pb-2">
+          <p className="w-28 text-center text-base text-dark-black">記録の数</p>
+          <div className="flex grow justify-evenly text-center">
+            <p className="text-center text-base text-dark-black">20日</p>
+          </div>
         </div>
       </div>
     </div>
