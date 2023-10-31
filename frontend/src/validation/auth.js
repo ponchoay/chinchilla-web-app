533f0290878d414a2cf2470add4ee59074e434cf
ponchoay <poncho1122c@gmail.com>
2023-10-31 18:59:15 +0900

    add:パスワード変更用のフォームを追加

--- frontend/src/validation/auth.js
+++ frontend/src/validation/auth.js
@@ -10,3 +10,24 @@ export const userSchema = z.object({
     .nonempty('パスワードは必須です')
     .min(6, 'パスワードは6文字以上で入力してください')
 })
+
+export const passwordChangeSchema = z
+  .object({
+    currentPassword: z
+      .string()
+      .nonempty('パスワードは必須です')
+      .min(6, 'パスワードは6文字以上で入力してください'),
+    newPassword: z
+      .string()
+      .nonempty('パスワードは必須です')
+      .min(6, 'パスワードは6文字以上で入力してください')
+  })
+  .superRefine(({ currentPassword, newPassword }, ctx) => {
+    if (currentPassword === newPassword) {
+      ctx.addIssue({
+        path: ['newPassword'],
+        code: 'custom',
+        message: '現在のパスワードと異なるものを設定してください'
+      })
+    }
+  })
