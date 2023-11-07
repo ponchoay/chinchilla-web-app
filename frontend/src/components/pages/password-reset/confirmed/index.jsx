8fb5e7002ceb5148c8885d57aeef232880195872
ponchoay <poncho1122c@gmail.com>
2023-11-07 12:22:30 +0900

    remove:不要なコードを削除

--- frontend/src/components/pages/password-reset/confirmed/index.jsx
+++ frontend/src/components/pages/password-reset/confirmed/index.jsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 
-import { Button } from 'src/components/shared/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
