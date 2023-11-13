78e3a1f38a718f60bb245b43ac5e8010d05c51db
ponchoay <poncho1122c@gmail.com>
2023-11-13 17:31:02 +0900

    fix:フッターのレスポンシブ対応

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -7,32 +7,32 @@ import { AuthContext } from 'src/contexts/auth'
 export const Footer = () => {
   const { isSignedIn, currentUser } = useContext(AuthContext)
 
+  const navigationBottomItems = [
+    { key: 'mychinchilla', link: '/mychinchilla', icon: faHouse },
+    { key: 'careRecordCalendar', link: '/care-record-calendar', icon: faCalendarDays },
+    { key: 'weightChart', link: '/weight-chart', icon: faChartLine }
+  ]
+
   return (
     <footer className="fixed bottom-0 z-50 h-16 w-full bg-dark-blue">
-      <div className="mx-auto flex h-full max-w-screen-lg place-content-evenly items-center">
+      <div className="mx-auto flex h-full max-w-screen-md items-center justify-between">
         {isSignedIn && currentUser ? (
-          <div>
-            <Link href="/mychinchilla">
-              <FontAwesomeIcon
-                icon={faHouse}
-                className="mx-12 text-4xl text-ligth-white transition-colors duration-200 hover:text-slate-200"
-              />
-            </Link>
-            <Link href="/care-record-calendar">
-              <FontAwesomeIcon
-                icon={faCalendarDays}
-                className="mx-12 text-4xl text-ligth-white transition-colors duration-200 hover:text-slate-200"
-              />
-            </Link>
-            <Link href="/weight-chart">
-              <FontAwesomeIcon
-                icon={faChartLine}
-                className="mx-12 text-4xl text-ligth-white transition-colors duration-200 hover:text-slate-200"
-              />
-            </Link>
-          </div>
+          <>
+            {navigationBottomItems.map((item) => (
+              <Link
+                key={item.key}
+                href={item.link}
+                className="flex h-full w-1/3 items-center justify-center transition-colors duration-200 hover:bg-slate-200/50"
+              >
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  className="block text-center text-4xl text-ligth-white"
+                />
+              </Link>
+            ))}
+          </>
         ) : (
-          <p className="text-base text-ligth-white">© 2023 ponchoay</p>
+          <p className="m-auto text-base text-ligth-white">© 2023 ponchoay</p>
         )}
       </div>
     </footer>
