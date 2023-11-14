bc404bb219518ff95bedbc78af7ebf5df79cab04
ponchoay <poncho1122c@gmail.com>
2023-11-14 14:42:47 +0900

    fix:メールアドレス変更ページのレスポンシブ対応

--- frontend/src/components/pages/mypage/email-change/email-confirmation-sent/index.jsx
+++ frontend/src/components/pages/mypage/email-change/email-confirmation-sent/index.jsx
@@ -8,20 +8,20 @@ export const EmailChangeEmailConfirmationSentPage = () => {
   const { processUser } = useContext(AuthContext)
 
   return (
-    <div className="my-40 grid place-content-center place-items-center">
+    <div className="mx-3 my-28 grid place-content-center place-items-center">
       <h1 className="text-center text-2xl font-bold tracking-widest text-dark-blue">
         認証メール送信
       </h1>
-      <div className="mt-8 w-[500px] rounded-xl bg-ligth-white p-10">
+      <div className="mt-8 w-80 rounded-xl bg-ligth-white p-8 sm:w-[500px] sm:p-10">
         <div className="mx-auto flex h-32 w-32 items-center justify-center rounded-[50%] bg-light-blue">
           <FontAwesomeIcon icon={faPaperPlane} className="text-5xl font-bold text-ligth-white" />
         </div>
-        <p className="mt-10 text-center text-base text-dark-black">
+        <p className="mt-10 break-words text-justify text-base text-dark-black sm:text-center">
           {processUser}宛に
           <br />
           メールアドレス変更用のメールを送信しました。
         </p>
-        <p className="mt-5 text-center text-base text-dark-black">
+        <p className="mt-5 text-justify text-base text-dark-black sm:text-center">
           メール内のリンクをクリックして
           <br />
           メールアドレスの変更を完了してください。
