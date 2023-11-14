eb8c251b524274ec6d722a0460781ae56328bb67
ponchoay <poncho1122c@gmail.com>
2023-11-14 15:36:53 +0900

    fix:退会ページのレスポンシブ対応

--- frontend/src/components/pages/mypage/deactivate/index.jsx
+++ frontend/src/components/pages/mypage/deactivate/index.jsx
@@ -39,21 +39,19 @@ export const DeactivatePage = () => {
   }
 
   return (
-    <div className="my-40 grid place-content-center place-items-center">
+    <div className="mx-3 my-28 grid place-content-center place-items-center">
       <h1 className="text-center text-2xl font-bold tracking-widest text-dark-blue">
-        退会（アカウントを削除）
+        退会(アカウントを削除)
       </h1>
 
       {/* 注意書き */}
-      <div className="mt-8 w-[500px] rounded-xl bg-ligth-white py-8">
-        <p className="mb-5 text-center text-base text-dark-black">
+      <div className="mt-8 w-80 rounded-xl bg-ligth-white p-5 sm:w-[500px] sm:p-8">
+        <p className="mb-5 text-justify text-base text-dark-black">
           アカウントと保存されたデータを完全に削除します。
         </p>
 
-        <p className="text-center text-base text-dark-black">
-          アカウントを削除すると、
-          <br />
-          保存されたデータは復元することができません。
+        <p className="text-justify text-base text-dark-black">
+          アカウントを削除すると、保存されたデータは復元することができません。
         </p>
       </div>
 
@@ -62,7 +60,7 @@ export const DeactivatePage = () => {
         <Button
           btnType="submit"
           click={() => setIsModalOpen(true)}
-          addStyle="btn-secondary h-16 w-40 my-12"
+          addStyle="btn-secondary h-14 w-32 my-12"
         >
           退会する
         </Button>
