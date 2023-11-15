a46f4e495561da374da8c38e56938c917761ab67
ponchoay <poncho1122c@gmail.com>
2023-11-15 23:36:49 +0900

    fix:Font awesomeが巨大化するバグを修正

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -6,6 +6,11 @@ import { AuthProvider } from 'src/contexts/auth'
 
 const kosugi = Kosugi({ weight: ['400'], subsets: ['latin'] })
 
+// font awesome巨大化回避
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 export default function App({ Component, pageProps }) {
   return (
     <>
