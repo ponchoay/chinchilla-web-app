86769e53bd7054b8d3f53e92d453f115a3977df4
ponchoay <poncho1122c@gmail.com>
2023-11-17 16:53:48 +0900

    fix:Calendarコンポーネントの不要なコードをコメントアウト

--- frontend/src/components/pages/care-record-calendar/calendar.jsx
+++ frontend/src/components/pages/care-record-calendar/calendar.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+// import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 import ja from 'date-fns/locale/ja'
 
@@ -84,8 +84,8 @@ function Calendar({ allCares, className, classNames, showOutsideDays = true, ...
         ...classNames
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />
+        // IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        // IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />
       }}
       {...props}
     />
