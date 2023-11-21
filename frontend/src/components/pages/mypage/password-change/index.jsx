352d740d9c94a986080b7af73e3f95d3ae19446a
ponchoay <poncho1122c@gmail.com>
2023-11-21 17:32:00 +0900

    fix:パスワード変更ページの入力フォームをRhfInputFormコンポーネントに置き換え

--- frontend/src/components/pages/mypage/password-change/index.jsx
+++ frontend/src/components/pages/mypage/password-change/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { updatePassword } from 'src/lib/api/auth'
 
@@ -7,17 +6,16 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { passwordChangeSchema } from 'src/validation/auth'
 
 import { PageTitle } from 'src/components/shared/PageTittle'
+import { RhfInputForm } from 'src/components/shared/RhfInputForm'
 import { Button } from 'src/components/shared/Button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 export const PasswordChangePage = () => {
   const router = useRouter()
 
   const {
-    register,
     handleSubmit,
-    formState: { dirtyFields, errors }
+    control,
+    formState: { dirtyFields }
   } = useForm({
     defaultValues: { currentPassword: '', newPassword: '' },
     resolver: zodResolver(passwordChangeSchema)
@@ -45,93 +43,43 @@ export const PasswordChangePage = () => {
     }
   }
 
-  // 現在のパスワード表示/非表示切り替え
-  const [isRevealCurrentPassword, setIsRevealCurrentPassword] = useState(false)
-  const toggleCurremntPassword = () => {
-    setIsRevealCurrentPassword((prevState) => !prevState)
-  }
-
-  // 新しいパスワード表示/非表示切り替え
-  const [isRevealNewPassword, setIsRevealNewPassword] = useState(false)
-  const toggleNewPassword = () => {
-    setIsRevealNewPassword((prevState) => !prevState)
-  }
-
   return (
-    <div className="mx-3 my-28 grid place-content-center place-items-center">
+    <div className="mx-3 my-28 grid place-content-center place-items-center gap-y-6">
       <PageTitle pageTitle="パスワードの変更" />
       <form
         noValidate
         onSubmit={handleSubmit(onSubmit)}
-        className="grid place-content-center place-items-center"
+        className="grid place-content-center place-items-center gap-y-6"
       >
-        <h3 className="my-6 text-base text-dark-black">
+        <h3 className="text-base text-dark-black">
           パスワードは6文字以上の半角英数字で入力してください
         </h3>
 
         {/* 現在のパスワード */}
-        <div className="form-control my-6 h-32 w-80 sm:w-96">
-          <label htmlFor="currentPassword" className="label">
-            <span className="text-base text-dark-black">現在のパスワード</span>
-          </label>
-          <div className="flex items-center">
-            <div className="relative">
-              <input
-                id="currentPassword"
-                type={isRevealCurrentPassword ? 'text' : 'password'}
-                {...register('currentPassword')}
-                placeholder="CurrentPassword"
-                className="input input-bordered input-primary input-md w-80 border-dark-blue bg-ligth-white text-base text-dark-black sm:w-96"
-              />
-              <span
-                onClick={toggleCurremntPassword}
-                role="presentation"
-                className="absolute right-3 top-3"
-              >
-                {isRevealCurrentPassword ? (
-                  <FontAwesomeIcon icon={faEye} />
-                ) : (
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                )}
-              </span>
-              {errors.currentPassword && (
-                <p className="label text-base text-dark-pink">{errors.currentPassword.message}</p>
-              )}
-            </div>
-          </div>
-        </div>
+        <RhfInputForm
+          htmlFor="currentPassword"
+          label="現在のパスワード"
+          id="currentPassword"
+          type="password"
+          autoComplete="current-password webauthn"
+          name="currentPassword"
+          control={control}
+          placeholder="currentPassword"
+          passwordForm={true}
+        />
 
         {/* 新しいパスワード */}
-        <div className="form-control mb-6 h-32 w-80 sm:w-96">
-          <label htmlFor="newPassword" className="label">
-            <span className="text-base text-dark-black">新しいパスワード</span>
-          </label>
-          <div className="flex items-center">
-            <div className="relative">
-              <input
-                id="newPassword"
-                type={isRevealNewPassword ? 'text' : 'password'}
-                {...register('newPassword')}
-                placeholder="NewPassword"
-                className="input input-bordered input-primary input-md w-80 border-dark-blue bg-ligth-white text-base text-dark-black sm:w-96"
-              />
-              <span
-                onClick={toggleNewPassword}
-                role="presentation"
-                className="absolute right-3 top-3"
-              >
-                {isRevealNewPassword ? (
-                  <FontAwesomeIcon icon={faEye} />
-                ) : (
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                )}
-              </span>
-              {errors.newPassword && (
-                <p className="label text-base text-dark-pink">{errors.newPassword.message}</p>
-              )}
-            </div>
-          </div>
-        </div>
+        <RhfInputForm
+          htmlFor="newPassword"
+          label="新しいパスワード"
+          id="newPassword"
+          type="password"
+          autoComplete="current-password webauthn"
+          name="newPassword"
+          control={control}
+          placeholder="newPassword"
+          passwordForm={true}
+        />
 
         {/* 保存 */}
         <Button
