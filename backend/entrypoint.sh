653dc92add865f99edd4408b84f2f2b0a0e8439f
ponchoay <poncho1122c@gmail.com>
2023-12-18 13:57:32 +0900

    fix: 本番環境用に修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,9 +4,19 @@ set -e
 # Rails特有の問題を解決するためのコマンド
 rm -f /backend/tmp/pids/server.pid
 
-# ローカル環境用
-# 初回コンテナ起動時のマイグレーションエラーを回避するため
-rails db:migrate RAILS_ENV=development
+if [ "$RAILS_ENV" = "production" ]; then
+  # 本番環境用
+  # 本番環境（AWS ECS）への初回デプロイ時に利用
+  # 初回デプロイ後にコメントアウトする
+  # bundle exec rails db:create
+  # --------------------------------------
+  bundle exec rails db:migrate
+else
+  # ローカル環境用
+  # 初回コンテナ起動時のマイグレーションエラーを回避するため
+  # 初回コンテナ起動後にコメントアウトする
+  bundle exec rails db:migrate
+fi
 
 # サーバー実行(DockerfileのCMDをセット)
 exec "$@"
