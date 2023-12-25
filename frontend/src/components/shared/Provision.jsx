74e26ae95838f4e89c2af4d6b6cee337470026c9
ponchoay <poncho1122c@gmail.com>
2023-12-25 12:29:36 +0900

    add: 利用規約ページの追加

---
+++ frontend/src/components/shared/Provision.jsx
@@ -0,0 +1,7 @@
+export const Provision = ({ text }) => {
+  return (
+    <h2 className="mb-2 border-b border-solid border-b-dark-blue pb-1 text-xl font-bold tracking-widest text-dark-blue ">
+      {text}
+    </h2>
+  )
+}
