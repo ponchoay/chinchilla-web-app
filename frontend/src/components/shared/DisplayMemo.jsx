1db3e616cf8072fe6912f8a8bc25ca6973706ecf
ponchoay <poncho1122c@gmail.com>
2023-12-27 00:51:25 +0900

    fix: チンチラプロフィールページのデザイン修正

--- frontend/src/components/shared/DisplayMemo.jsx
+++ frontend/src/components/shared/DisplayMemo.jsx
@@ -6,10 +6,12 @@ export const DisplayMemo = ({ contents }) => {
     <div>
       <div className="flex px-1 py-2">
         <FontAwesomeIcon icon={faFilePen} className="mx-1 pt-[3px] text-lg text-dark-black" />
-        <p className=" text-left text-base text-dark-black">メモ</p>
+        <p className=" text-left text-sm text-dark-black sm:text-base">メモ</p>
       </div>
       <div className="h-80 w-80 overflow-y-auto rounded-xl bg-ligth-white p-5 sm:h-96 sm:w-[500px]">
-        <p className="whitespace-pre-wrap text-left text-base text-dark-black">{contents}</p>
+        <p className="whitespace-pre-wrap text-left text-sm text-dark-black sm:text-base">
+          {contents}
+        </p>
       </div>
     </div>
   )
