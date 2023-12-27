0531813aa48f339459d83bcaae8fba7b521ac142
ponchoay <poncho1122c@gmail.com>
2023-12-27 21:05:57 +0900

    fix: バックエンド側のchinchilla_memoの字数制限を変更

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_14_044025) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_27_120007) do
   create_table "cares", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.date "care_day", null: false
     t.string "care_food"
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_044025) do
     t.string "chinchilla_sex", null: false
     t.date "chinchilla_birthday"
     t.date "chinchilla_met_day"
-    t.string "chinchilla_memo", limit: 200
+    t.string "chinchilla_memo", limit: 500
     t.string "chinchilla_image"
     t.bigint "user_id"
     t.datetime "created_at", null: false
