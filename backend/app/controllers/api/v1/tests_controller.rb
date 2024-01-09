045d0913fce173c0c4969a29b0a50cb1987b8602
ponchoay <poncho1122c@gmail.com>
2024-01-09 14:47:13 +0900

    add: testコントローラーにステータスコードを追加

--- backend/app/controllers/api/v1/tests_controller.rb
+++ backend/app/controllers/api/v1/tests_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TestsController < ApplicationController
   def index
-    render json: { message: 'Hello, world!!' }
+    render json: { message: 'Hello, world!!' }, status: :ok
   end
 end
