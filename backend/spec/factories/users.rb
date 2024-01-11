5950ec6cc8fcc44b092e3c42dd72cfe4da48cb53
ponchoay <poncho1122c@gmail.com>
2024-01-11 17:00:26 +0900

    fix: Userモデルのテストデータを修正

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     provider { 'email' }
     uid { SecureRandom.uuid }
-    sequence(:email) { |n| 'test+#{n}@example.com' }
+    # シングルクォートだと式展開が行われない
+    sequence(:email) { |n| "test+#{n}@example.com" }
     password { 'password' }
   end
 end
