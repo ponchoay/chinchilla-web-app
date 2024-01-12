acc20ea65ed36a5712eb9cfa166d431d3848ad13
ponchoay <poncho1122c@gmail.com>
2024-01-12 16:30:12 +0900

    fix: caresコントローラーにログイン中のユーザー所有のデータか確認する処理を追加

--- backend/app/controllers/api/v1/cares_controller.rb
+++ backend/app/controllers/api/v1/cares_controller.rb
@@ -2,26 +2,31 @@ class Api::V1::CaresController < ApplicationController
   # ログイン状態の確認
   before_action :authenticate_api_v1_user!
 
+  # ログイン中のユーザー所有のデータか確認
+  before_action :set_chinchilla_for_get, only: %i[all_cares weight_cares]
+  before_action :set_chinchilla_for_post, only: [:create]
+  before_action :set_care, only: %i[update destroy]
+
   # ステータスコード404の共通処理
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   # お世話記録 一覧
   def all_cares
-    cares = Care.where(chinchilla_id: params[:chinchilla_id])
+    cares = @chinchilla.cares
     render json: cares, status: :ok
   end
 
   # 体重 一覧（nullを除いて日付の古い順）
   def weight_cares
-    cares = Care.where(chinchilla_id: params[:chinchilla_id])
-                .where.not(care_weight: nil)
-                .order(:care_day)
+    cares = @chinchilla.cares
+                       .where.not(care_weight: nil)
+                       .order(:care_day)
     render json: cares.as_json(only: %w[care_day care_weight]), status: :ok
   end
 
   # お世話記録 作成
   def create
-    care = Care.new(create_care_params)
+    care = @chinchilla.cares.new(create_care_params)
 
     if care.save
       # 成功した場合、ステータス201を返す
@@ -34,10 +39,9 @@ class Api::V1::CaresController < ApplicationController
 
   # お世話記録 更新
   def update
-    care = Care.find(params[:id])
-    if care.update(update_care_params)
+    if @care.update(update_care_params)
       # 成功した場合、ステータス200を返す
-      render json: care, status: :ok
+      render json: @care, status: :ok
     else
       # エラー文を取得し、ステータス422を返す
       render json: { errors: ['お世話の記録の更新に失敗しました'] }, status: :unprocessable_entity
@@ -46,8 +50,7 @@ class Api::V1::CaresController < ApplicationController
 
   # お世話記録 削除
   def destroy
-    care = Care.find(params[:id])
-    care.destroy
+    @care.destroy
     head :no_content
   end
 
@@ -71,7 +74,29 @@ class Api::V1::CaresController < ApplicationController
     )
   end
 
+  def set_chinchilla_for_get
+    # chinchilla_idはクエリパラメータとして送信されるので、params[:chinchilla_id]
+    @chinchilla = current_api_v1_user.chinchillas.find_by(id: params[:chinchilla_id])
+    record_not_found unless @chinchilla
+  end
+
+  def set_chinchilla_for_post
+    # chinchilla_idはリクエストボディに含まれて送信されるので、params[:care][:chinchilla_id]
+    @chinchilla = current_api_v1_user.chinchillas.find_by(id: params[:care][:chinchilla_id])
+    chinchilla_not_found unless @chinchilla
+  end
+
+  def set_care
+    care = Care.find_by(id: params[:id])
+    record_not_found unless care && current_api_v1_user.chinchillas.exists?(care.chinchilla_id)
+    @care = care
+  end
+
   def record_not_found
     render json: { errors: ['指定されたお世話の記録が見つかりませんでした'] }, status: :not_found
+  end
+
+  def chinchilla_not_found
+    render json: { errors: ['指定されたチンチラが見つかりませんでした'] }, status: :not_found
   end
 end
