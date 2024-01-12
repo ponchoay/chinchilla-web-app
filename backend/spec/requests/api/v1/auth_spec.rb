2fdee5489194ab4f7b6989a29c3fc803bf65f620
ponchoay <poncho1122c@gmail.com>
2024-01-12 22:13:36 +0900

    add:サインイン機能のテストコードを追加

--- backend/spec/requests/api/v1/auth_spec.rb
+++ backend/spec/requests/api/v1/auth_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe '/api/v1/auth', type: :request do
+  let!(:user) { create(:user) }
+
+  # JSONデータの中身を確認するためのkeyの配列
+  let(:header_keys) do
+    %w[uid client access-token]
+  end
+
+  before do
+    # ヘルパーモジュールのサインインメソッドを使用
+    @headers = sign_in(user)
+  end
+
   describe 'POST /api/v1/auth' do
     let(:confirm_success_url) { 'http://localhost:3010' }
     let(:email) { 'test@example.com' }
@@ -46,6 +58,45 @@ RSpec.describe '/api/v1/auth', type: :request do
 
       it 'ステータスコード422が返ってくること' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'POST /api/v1/auth/sign_in' do
+    context '正しいパラメーターでサインインしたとき' do
+      before do
+        post '/api/v1/auth/sign_in', params: { email: user.email, password: user.password }
+      end
+
+      it 'ステータスコード200が返ってくること' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'レスポンスヘッダーにトークン情報があること' do
+        header_keys.each do |key|
+          expect(response.header).to have_key(key)
+        end
+      end
+
+      it 'トークンが有効化されていること' do
+        get '/api/v1/auth/validate_token', headers: @headers
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context '誤ったパラメーターでサインインしたとき' do
+      before do
+        post '/api/v1/auth/sign_in', params: { email: user.email, password: 'error_password' }
+      end
+
+      it 'ステータスコード401が返ってくること' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'レスポンスヘッダーにトークン情報がないこと' do
+        header_keys.each do |key|
+          expect(response.header).not_to have_key(key)
+        end
       end
     end
   end
