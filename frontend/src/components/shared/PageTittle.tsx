d036e603ba0c7ddb944e5c35cc2623a4cfc55cb5
ponchoay <poncho1122c@gmail.com>
2024-01-13 13:33:16 +0900

    add: 利用規約及びプライバシーポリシーページをTypeScriptに移行

--- frontend/src/components/shared/PageTittle.tsx
+++ frontend/src/components/shared/PageTittle.tsx
@@ -1,4 +1,8 @@
-export const PageTitle = ({ pageTitle }) => {
+type Props = {
+  pageTitle: string
+}
+
+export const PageTitle = ({ pageTitle }: Props) => {
   return (
     <h1 className="text-center text-xl font-bold tracking-widest text-dark-blue sm:text-2xl">
       {pageTitle}
