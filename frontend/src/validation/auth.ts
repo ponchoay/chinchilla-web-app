09e4bf5e91bcdf64ae4cecd234ec46bb6593fb1a
ponchoay <poncho1122c@gmail.com>
2024-01-15 17:53:22 +0900

    add: AuthのバリデーションスキーマをTypeScriptに移行

--- frontend/src/validation/auth.ts
+++ frontend/src/validation/auth.ts
@@ -1,13 +1,9 @@
 import { z } from 'zod'
 
 export const userSchema = z.object({
-  email: z
-    .string()
-    .nonempty('メールアドレスは必須です')
-    .email('メールアドレスの形式ではありません'),
+  email: z.string().min(1, 'メールアドレスは必須です').email('メールアドレスの形式ではありません'),
   password: z
     .string()
-    .nonempty('パスワードは必須です')
     .min(6, 'パスワードは6文字以上で入力してください')
     .refine((value) => !/\s/.test(value), 'スペースは使用できません')
     .refine((value) => !/[^\u0020-\u007E]+/.test(value), '全角文字は使用できません')
@@ -20,7 +16,6 @@ export const sendPasswordResetMailSchema = z.object({
 export const resetPasswordSchema = z.object({
   password: z
     .string()
-    .nonempty('パスワードは必須です')
     .min(6, 'パスワードは6文字以上で入力してください')
     .refine((value) => !/\s/.test(value), 'スペースは使用できません')
     .refine((value) => !/[^\u0020-\u007E]+/.test(value), '全角文字は使用できません')
@@ -30,13 +25,11 @@ export const passwordChangeSchema = z
   .object({
     currentPassword: z
       .string()
-      .nonempty('パスワードは必須です')
       .min(6, 'パスワードは6文字以上で入力してください')
       .refine((value) => !/\s/.test(value), 'スペースは使用できません')
       .refine((value) => !/[^\u0020-\u007E]+/.test(value), '全角文字は使用できません'),
     newPassword: z
       .string()
-      .nonempty('パスワードは必須です')
       .min(6, 'パスワードは6文字以上で入力してください')
       .refine((value) => !/\s/.test(value), 'スペースは使用できません')
       .refine((value) => !/[^\u0020-\u007E]+/.test(value), '全角文字は使用できません')
