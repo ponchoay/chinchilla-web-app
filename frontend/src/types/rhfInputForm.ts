f83e272de1aeeb1cd74f5205b70b5d620ddda311
ponchoay <poncho1122c@gmail.com>
2024-01-15 15:08:58 +0900

    add: RHFInputコンポーネントをTypeScriptに移行

---
+++ frontend/src/types/rhfInputForm.ts
@@ -0,0 +1,11 @@
+export type rhfInputFormType = {
+  htmlFor: string
+  label: string
+  explanation: string | null
+  id: string
+  type: string
+  autoComplete: string
+  name: string
+  placeholder: string
+  passwordForm: boolean
+}
