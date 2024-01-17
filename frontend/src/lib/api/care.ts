2e990699796860d4fc6c9151b5f701f9c112a6c9
ponchoay <poncho1122c@gmail.com>
2024-01-17 17:26:35 +0900

    fix: APIクライアントを修正

--- frontend/src/lib/api/care.ts
+++ frontend/src/lib/api/care.ts
@@ -1,18 +1,41 @@
 import Cookies from 'js-cookie'
+import useSWR from 'swr'
 import { client } from 'src/lib/api/client'
+
+import type { CreateCareType, UpdateCareType } from 'src/types/care'
 
 // 機能&リクエストURL
 
+const fetchWithToken = (url: string) => {
+  const accessToken = Cookies.get('_access_token')
+  const clientToken = Cookies.get('_client')
+  const uid = Cookies.get('_uid')
+
+  if (!accessToken || !client || !uid) return
+
+  return client
+    .get(url, {
+      headers: {
+        'access-token': accessToken,
+        client: clientToken,
+        uid: uid
+      }
+    })
+    .then((res) => res.data)
+}
+
 // お世話記録 一覧(全部)
-export const getAllCares = (selectedChinchillaId: number) => {
-  if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return
-  return client.get(`/all_cares?chinchilla_id=${selectedChinchillaId}`, {
-    headers: {
-      'access-token': Cookies.get('_access_token'),
-      client: Cookies.get('_client'),
-      uid: Cookies.get('_uid')
-    }
-  })
+export const useAllCares = (chinchillaId: number) => {
+  const { data, error, isLoading } = useSWR(
+    chinchillaId !== 0 ? `/all_cares?chinchilla_id=${chinchillaId}` : null,
+    fetchWithToken
+  )
+
+  return {
+    allCares: data,
+    isLoading,
+    isError: error
+  }
 }
 
 // 体重 一覧
@@ -28,27 +51,25 @@ export const getWeightCares = (selectedChinchillaId: number) => {
 }
 
 // お世話記録 作成
-export const createCare = (params: FormData) => {
+export const createCare = (params: CreateCareType) => {
   if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return
   return client.post('/cares', params, {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),
-      uid: Cookies.get('_uid'),
-      'content-type': 'multipart/form-data'
+      uid: Cookies.get('_uid')
     }
   })
 }
 
 // お世話記録更新
-export const updateCare = (careId: number, params: FormData) => {
+export const updateCare = (careId: number, params: UpdateCareType) => {
   if (!Cookies.get('_access_token') || !Cookies.get('_client') || !Cookies.get('_uid')) return
   return client.put(`/cares/${careId}`, params, {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),
-      uid: Cookies.get('_uid'),
-      'content-type': 'multipart/form-data'
+      uid: Cookies.get('_uid')
     }
   })
 }
