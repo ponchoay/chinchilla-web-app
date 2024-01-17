fb74468fce46856be1dcd8dde2ab581fc653268a
ponchoay <poncho1122c@gmail.com>
2024-01-17 17:24:45 +0900

    add: POST/PUTリクエスト用の型を追加

--- frontend/src/types/care.ts
+++ frontend/src/types/care.ts
@@ -11,3 +11,27 @@ export type AllCaresType = {
   careMemo: string
   chinchillaId: number
 }
+
+export type CreateCareType = {
+  careDay: string
+  careFood: string
+  careToilet: string
+  careBath: string
+  carePlay: string
+  careWeight: number | null
+  careTemperature: number | null
+  careHumidity: number | null
+  careMemo: string
+  chinchillaId: number
+}
+
+export type UpdateCareType = {
+  careFood: string
+  careToilet: string
+  careBath: string
+  carePlay: string
+  careWeight: number | null
+  careTemperature: number | null
+  careHumidity: number | null
+  careMemo: string
+}
