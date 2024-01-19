62f2971c783c39edfdcf90aba5581d43fe7fd381
ponchoay <poncho1122c@gmail.com>
2024-01-19 16:36:27 +0900

    add: マルチデバイス用にfaviconを追加

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -8,7 +8,12 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
         <meta name="description" content={description} />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
