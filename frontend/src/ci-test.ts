ba0e50b8e754c78175bc1cd1ad3f513d85640381
ponchoay <ponchoay@users.noreply.github.com>
2024-02-11 07:49:02 +0000

    Apply Prettier Changes

--- frontend/src/ci-test.ts
+++ frontend/src/ci-test.ts
@@ -2,18 +2,10 @@ import { useState } from 'react'
 
 export const CiTest = () => {
   // パスワード表示/非表示切り替え
-        const [isRevealPassword, setIsRevealPassword] = useState(false)
+  const [isRevealPassword, setIsRevealPassword] = useState(false)
   const togglePassword = () => {
-
-
-
-
-        setIsRevealPassword((prevState) => !prevState)
+    setIsRevealPassword((prevState) => !prevState)
   }
 
   return { isRevealPassword, setIsRevealPassword, togglePassword }
-
-
-
-
 }
