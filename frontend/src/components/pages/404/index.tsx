c9fa82121b4e21f6a829ee0b671ab62b9302f511
ponchoay <poncho1122c@gmail.com>
2024-02-12 01:17:22 +0900

    add: ESLintでエラーが出るダミーコードを追加

--- frontend/src/components/pages/404/index.tsx
+++ frontend/src/components/pages/404/index.tsx
@@ -1,8 +1,11 @@
-import { faCalendarXmark } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
+import { faCalendarXmark } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { Button } from 'src/components/shared/Button'
+
 import { PageTitle } from 'src/components/shared/PageTittle'
 
 export const Custom404Page = () => {
