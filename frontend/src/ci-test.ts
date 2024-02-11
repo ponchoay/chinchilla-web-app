bdfc1a1d2f99db536ec3cd933becd85c4a3132b7
ponchoay <poncho1122c@gmail.com>
2024-02-12 00:50:57 +0900

    add: コード整形が必要なダミーコードを追加

--- frontend/src/ci-test.ts
+++ frontend/src/ci-test.ts
@@ -2,9 +2,16 @@ import { useState } from 'react'
 
 export const CiTest = () => {
   // パスワード表示/非表示切り替え
-  const [isRevealPassword, setIsRevealPassword] = useState(false)
+      const [isRevealPassword, setIsRevealPassword] = useState(false)
   const togglePassword = () => {
+
+
+
     setIsRevealPassword((prevState) => !prevState)
+
+
+
+
   }
 
   return { isRevealPassword, setIsRevealPassword, togglePassword }
